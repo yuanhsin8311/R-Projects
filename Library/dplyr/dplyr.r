9d70e61d2a02875c6a7c7642d09e7fb29965c66b
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-01-24 10:39:26 -0800

    Update dplyr.r

--- Library/dplyr/dplyr.r
+++ Library/dplyr/dplyr.r
@@ -1,1 +1,17 @@
+# Data Manipulation Package
+
+# Read text file
+mydata = read.table("mydata.txt",header=FALSE,sep="")
+# Print data frame
+mydata
+# Help
+? read.table
+
+# CSV file
+# sep ="\t"  ","  " "
+
+data = read.csv("data.csv",header=TRUE, sep=",")
+
+
+
 
