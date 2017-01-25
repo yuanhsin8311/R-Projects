d01b6555254fe9051d28c53f9338102d40915fd6
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-01-24 19:29:36 -0800

    Update dplyr.r

--- Library/dplyr/dplyr.r
+++ Library/dplyr/dplyr.r
@@ -11,7 +11,21 @@ mydata
 data = read.csv("data.csv",header=TRUE, sep=",")
 
 ## Dataset: nycflights13
-# this dataset contains 336,776 flights that departed from New York City in 2013.
+#  This dataset contains 336,776 flights that departed from New York City in 2013. 
+#  Source from US Bureau of Transportation Statistics
 
+# Data Shape: number of row, number of column
+dim(flights) 
+# Data Structure: column's data type
+str(flights)
 
+# Print last 5 rows of data
+tail(flights,n=5)
+# Print first 5 rows
+head(flights)
+
+## dplyr
+# filter()
+filter_1 <- filter(flights,months==9 | months==10 ,day<2)
+# slice()
 
