4162247b7afa744f94f7c5ce73abf8974b1302a3
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-01-25 11:17:29 -0800

    Create Twitter Sentiment Analysis.r

---
+++ Library/Twitter Sentiment Analysis.r
@@ -0,0 +1,12 @@
+## Amazon Customer Service Analysis
+## (Objective) Discover the positive and negative opinions about a product or a brand 
+# Install package
+library(twitteR)
+# Get Twitter Authentication
+api_key <- "tnBE3xfBFKn4CjMqHJMNPROvJ"
+api_secret <- "ZubR8tctyB827d53Xuj4qoUlCVlnGFHyjCmqm9Z5IBBz3frbuS"
+access_token <- "785536933-LlXiXJEnkb89YTpbLWQl95BbHY3YobrBPlbkMJyf"
+access_token_secret <- "NjGZ4NXMoNi2ll7L0WZL9w6QxJx5TXM4ysW3fjVFjLi9F"
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+# 
+user <- getUser(""
